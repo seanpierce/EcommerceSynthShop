d4f5453384fb4af7ed679a67b6aa84f050d7be6c
Sean Pierce and Tanner Eustice <tanner@>
2017-07-11 09:26:54 -0700

    add show page for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    # @cart = current_order.carts.new
+  end
+  def show
+    @product = Product.find(params[:id])
     @cart = current_order.carts.new
   end
 end
