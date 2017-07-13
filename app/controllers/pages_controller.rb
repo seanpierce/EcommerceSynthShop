f9ca4a8d8fb895ad29b2875ee2e1621d6058bda7
Sean Pierce and Tanner Eustice <tanner@>
2017-07-13 14:14:43 -0700

    cart bug fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
 
+
   def show
+    current_order
     @products = []
     current_user.orders.each do |order|
-      order.carts.each do |item|
-         @products.push(Product.find(item.product_id))
+      if order.status === "processing"
+        order.carts.each do |item|
+           @products.push(Product.find(item.product_id))
+        end
       end
     end
   end
