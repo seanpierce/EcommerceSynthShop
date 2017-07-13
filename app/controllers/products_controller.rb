78a4c24d863efe2718c4dea8b35fb17d48ab81a8
Sean Pierce and Tanner Eustice <tanner@>
2017-07-13 16:11:27 -0700

    refactor display_purchased_with to the product model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,12 +9,8 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @purchased_with = []
-    freq = @product.purchased_with.split(',').inject(Hash.new(0)) { |h,v| h[v] += 1; h }
-    purchased_with_ids = freq.sort_by { |key, value| value }.reverse[0..2]
-    purchased_with_ids.each do |id|
-      @purchased_with.push(Product.find(id[0]))
-    end
+    @purchased_with = Product.display_purchased_with(@product)
+
     if current_user
       @cart = current_order.carts.new
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -15,4 +15,15 @@ class Product < ApplicationRecord
       item.save
     end
   end
+
+  def self.display_purchased_with(product)
+    purchased_with = []
+    freq = product.purchased_with.split(',').inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    purchased_with_ids = freq.sort_by { |key, value| value }.reverse[0..2]
+    purchased_with_ids.each do |id|
+      purchased_with.push(Product.find(id[0]))
+    end
+
+    purchased_with
+  end
 end
